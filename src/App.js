a0c736d4ae0c2031b29858d247f42b5de576c6eb
umesh-chowdary-learing <learning.umeshchowdary@gmail.com>
2024-08-01 13:34:43 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
+import homePage from "./pages/homePage";
+import {  Route, Routes } from 'react-router-dom';
+import HeaderCmp from "./components/headerCmp";
+import EventManagerPage from "./pages/EventManagerPage";
+function App() {
+  
+ 
+  return (
+    <>
+    <HeaderCmp>
+    <Routes>
+      <Route path='/' exact Component={EventManagerPage}/>
+      <Route path="/eventManagerPage" Component={EventManagerPage}/>
+      <Route path="/eventManagerPage" Component={EventManagerPage}/>
+    </Routes>
+    </HeaderCmp>
+    
+</>
+    
+  );
+}
+
+export default App;
+
+
+
+
+
+
 
